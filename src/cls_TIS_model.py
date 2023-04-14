e9f9f98701f7e6cce4c1af60afe769ff1bbaf3ef
Yubin Kim <127487336+ubinKim@users.noreply.github.com>
2023-04-14 13:43:13 +0900

    Add files via upload

---
+++ src/cls_TIS_model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+class TISv1(nn.Module):
+    def __init__(self, dropout, hidden_layer):
+        super(TISv1, self).__init__()
+        self.features = \
+            nn.Sequential(\
+                # 1
+                nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(7, 4)),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=(3, 1)),
+                # 2
+                nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 1)),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=(3, 1)),
+                # 3
+                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 1)),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=(3, 1))
+                )
+
+        self.classifier = \
+            nn.Sequential(
+                nn.Linear(in_features=64, out_features=hidden_layer),
+                nn.ReLU(),
+                nn.Dropout(p=dropout),
+                nn.Linear(in_features=hidden_layer, out_features=2))
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+# Test run
+if __name__ == "__main__":
+    test_input = torch.randn((1, 1, 60, 4))
+    model = TISv1(dropout=0.5, hidden_layer=32)
+    out = model(test_input)
+    print(out.shape)
